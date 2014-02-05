57c813899bb63cb631878efcc34820a79d8a5794
Donovan McKelvey <programmingmind@gmail.com>
2014-02-04 19:01:48 -0800

    binary GCD algorithm

--- gcd.cu
+++ gcd.cu
@@ -47,15 +47,15 @@ __device__ int cmp(uint32_t num1[], uint32_t num2[]) {
 	return EQ;
 }
 
-// requires that num1 >= num2
-__device__ void cuSubstract(uint32_t num1[], uint32_t num2[], uint32_t res[]) {
+// requires that num1 >= num2, num1 -= num2
+__device__ void cuSubstract(uint32_t num1[], uint32_t num2[]) {
    for (int i = 0; i < SIZE: i++) {
 	   if (num2[i] == min(num1[i], num2[i])) {
 		   // normal subtraction
-			res[i] = num1[i] - num2[i];
+			num1[i] = num1[i] - num2[i];
 		} else {
 		   // num1 - num2 == -1 * (num2 - num1)
-			res[i] = 1 + ~(num2[i] - num1[i]);
+			num1[i] = 1 + ~(num2[i] - num1[i]);
 			
 			if (num1[i+1] == 0)
 			   num2[i+1]++;
@@ -65,12 +65,48 @@ __device__ void cuSubstract(uint32_t num1[], uint32_t num2[], uint32_t res[]) {
 	}
 }
 
-__device__ void gcd(uint32_t num1[], uint32_t num2[], uint32_t gcd[]) {
+// eulers gcd algorithm without modulus
+__device__ void slow_gcd(uint32_t num1[], uint32_t num2[]) {
    int compare;
 	while ((compare = cmp(num1, num2)) != EQ) {
 	   if (compare == GT)
-		   cuSubstract(num1, num2, res);
+		   cuSubstract(num1, num2);
 		else
-		   cuSubtract(num2, num1, res);
+		   cuSubtract(num2, num1);
 	}
+}
+
+// Binary GCD algorithm
+// requires num1 > 0 and num2 > 0
+// sets either num1 or num2 to the gcd and returns the pointer to that num
+__device__ uint32_t* gcd(uint32_t *num1, uint32_t *num2) {
+   int shift, compare;
+	
+	for (shift = 0; ((num1[0] | num2[0]) & LOWBIT) == 0; ++shift) {
+	   shiftR1(num1);
+		shiftR1(num2);
+	}
+	
+	while ((num1[0] & 1) == 0)
+	   shiftR1(num1);
+	
+	do {
+	   while ((num2[0] & 1) == 0)
+		   shiftR1(num2);
+		
+		compare = cmp(num1, num2);
+		if (compare == EQ)
+		   break;
+		else (compare == GT) {
+		   uint32_t *t = num1;
+			num1 = num2;
+			num2 = t;
+		}
+		cuSubtract(num2, num1);
+	} while (1);
+	
+	while (shift--)
+	   shiftL1(num1);
+	
+	return num1;
 }
